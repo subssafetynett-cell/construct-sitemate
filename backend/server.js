be3522c3eb8f436910a515b567e68d74149b9974
jinsiyajasmin <jinsiyajasmin9@gmail.com>
2026-04-23 17:49:42 +0530

    Fixing Express 5 wildcard path error for CORS preflight

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Enable pre-flight for all routes using same options
+app.options("(.*)", cors(corsOptions)); // Enable pre-flight for all routes using same options
 
 app.use((req, res, next) => {
   next();
